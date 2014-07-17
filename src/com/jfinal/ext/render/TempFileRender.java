3b2cc56f7003fc1e5784c318cc46df3360f3eeb4
0x0001 <liaoxingliao@gmail.com>
2014-07-17 17:36:30 +0800

    增加临时文件下载渲染,文件被下载后会被删除

---
+++ src/com/jfinal/ext/render/TempFileRender.java
@@ -0,0 +1,35 @@
+package com.jfinal.ext.render;
+
+import com.jfinal.render.FileRender;
+
+import java.io.File;
+
+/**
+ * Created by 0x0001 on 14-7-17.
+ */
+public class TempFileRender extends FileRender {
+	private File file;
+	public TempFileRender(File file) {
+		super(file);
+		this.file = file;
+	}
+
+	public TempFileRender(String fileName) {
+		super(fileName);
+		this.file = new File(fileName);
+	}
+
+
+	@Override
+	public void render() {
+		try {
+			super.render();
+		} finally {
+			if (file == null) {
+				if (!file.delete()) {
+					file.deleteOnExit();
+				}
+			}
+		}
+	}
+}
